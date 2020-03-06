3ef58b05532c62cc108f5022afb5f50880a5f018
tako <0214.taiki@gmail.com>
2020-03-06 18:10:20 +0900

    delete over frame

--- logo/audio_node_logo.js
+++ logo/audio_node_logo.js
@@ -5,17 +5,6 @@ You can try p5.js from the following URL.
 https://editor.p5js.org/dannyrozin/sketches/r1djoVow7
 */
 
-const cornerCircleVertex = (x1, y1, x2, y2, x3, y3) => {
-  const pi = 2 * Math.acos(0);
-  const h = 4 * (Math.sqrt(2) - 1) / 3;
-  vertex(x1, y1);
-  bezierVertex(
-    x1 + h * (x2 - x1), y1 + h * (y2 - y1),
-    x3 + h * (x2 - x3), y3 + h * (y2 - y3),
-    x3, y3
-  );
-};
-
 const sinVezierVertex = (x1, x2, y, height, repeat, over) => {
   const pi = 2 * Math.acos(0);
   const div = 2 * repeat;
@@ -47,7 +36,8 @@ const getParam = (filename, width, aspect, weight, repeat) => {
   param.left = param.center_x - param.width * 0.5 + (param.weight + param.edge) * 0.5;
   param.width -= param.weight + param.edge;
   param.height = param.width * param.aspect;
-  param.outer_height = param.height + param.edge * 0.5;
+  const tmp = 1.5 * (param.height * 0.14) * 2 * (4 / 3);
+  param.outer_height = tmp + param.weight * 0.6;
   param.center_y = param.outer_height * 0.5;
   param.top = param.center_y - param.height * 0.5;
   return param;
@@ -58,33 +48,6 @@ const node = (param) => {
   strokeCap(SQUARE);
   noFill();
   stroke(255, 41, 137);
-  
-  // around
-  strokeWeight(param.weight);
-  beginShape();
-  cornerCircleVertex(
-    param.left, param.top + param.edge,
-    param.left, param.top,
-    param.left + param.edge, param.top
-  );
-  cornerCircleVertex(
-    param.left + param.width - param.edge, param.top,
-    param.left + param.width, param.top,
-    param.left + param.width, param.top + param.edge
-  );
-  endShape();
-  beginShape();
-  cornerCircleVertex(
-    param.left, param.top + param.height - param.edge,
-    param.left, param.top + param.height,
-    param.left + param.edge, param.top + param.height
-  );
-  cornerCircleVertex(
-    param.left + param.width - param.edge, param.top + param.height,
-    param.left + param.width, param.top + param.height,
-    param.left + param.width, param.top + param.height - param.edge
-  );
-  endShape();
   
   // sin wave
   strokeWeight(param.weight * 0.6);
@@ -114,6 +77,7 @@ function setup() {
   param = getParam("audio_node_logo.svg", size, 2/3, size * 0.092, 2);
   //param = getParam("audio_node_logo_small.svg", size, 1, size * 0.12, 1);
   createCanvas(param.outer_width, param.outer_height, SVG);
+  //background(0);
   node(param);
   save(param.filename);
 }
